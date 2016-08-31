c829cbe786153d659ddfb278f31af37c3b4503b2
Albert Wildeman <Albert Wildeman>
2016-08-30 21:13:14 -0500

    Started framework to read graph from .txt file.

---
+++ GraphReadLib.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+
+def txt_to_ndarray(filename):
+
+    graph = os.getcwd()
+    # dropbox_directory = "C:\\Users\\Albert\\Dropbox\\Algorithms\\Assignments\\"
+    # filepath = dropbox_directory + algoFileName
+    #
+    # n = 200
+    # Nodes = [0] * n
+    # Edges = [0] * n
+    #
+    # file_graph = open(filepath)
+    #
+    # for iLine, line in enumerate(file_graph):
+    #     x, *y = line.split("\t")
+    #     Nodes[iLine] = int(x) - 1
+    #     Edges[iLine] = list(int(i) - 1 for i in y[:-1])
+    #
+    # file_graph.close()
+    #
+    # graph = [Nodes, Edges]
+
+    return graph
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import numpy as np
+
+from GraphReadLib import txt_to_ndarray
+
+SCC_filename = "SCC.txt"
+
+G = txt_to_ndarray(SCC_filename)
+print(G)
