61e08e7f49fadb85a55de5b73a18a82f72636170
Albert Wildeman <albert.wildeman@gmail.com>
2016-09-01 20:10:47 -0500

    Code fully functional but very slow for large graph sizes.

--- KosarajuLib.py
+++ KosarajuLib.py
@@ -8,7 +8,12 @@ def kosaraju(G):
     # Calculate finishing time for each node, that is, the order is in which the second, forward, DFS loop will execute
     f = kosaraju_rev_dfs_loop(G)
 
-    leaders = kosaraju_fwd_dfs_loop(G, f)
+    # Reorder the nodes, in order of decreading finishing time
+    reordering = f.max() - f
+    G_reordered = reordering[G]
+
+    # Calculate the leader for each node
+    leaders = kosaraju_fwd_dfs_loop(G_reordered)
 
     return leaders
 
@@ -32,13 +37,14 @@ def kosaraju_rev_dfs_loop(G):
     for iNode in range(nNodes):
         if not explored[iNode]:
             # call DFS, but with the rows (ie, direction) of the edge list reversed
+            print("DFS_rev exploring node " + str(iNode))
             t = DFS_rev(G[:, ::-1], iNode, t, f, explored)
 
     return f
 
 def DFS_rev(G, s, t, f, explored):
 
-    stacksize = 1000
+    stacksize = 100000
     stack = np.zeros(stacksize, dtype=np.int32)
     stackexpl = np.zeros(stacksize, dtype=np.bool)
 
@@ -71,27 +77,50 @@ def DFS_rev(G, s, t, f, explored):
 
     return t
 
-def kosaraju_fwd_dfs_loop(G, f):
+
+def kosaraju_fwd_dfs_loop(G):
     # Perform the second DFS (depth-first search) loop on the graph G
 
-    # t is a counter that marks number of already-explored nodes.
-    # It will be used to determine the processing order of the nodes for the
-    # second (forward) DFS loop.
-    t = 0
+    # Get number of nodes from edge list G
+    nNodes = G.max() + 1
 
-    # # Get number of nodes from edge list G
-    # nNodes = G.max() + 1
-    #
-    # # initialize array that will hold the finishing times of all nodes
-    # f = np.zeros(nNodes, dtype=np.int32)
-    #
-    # # Initialize an array to keep track of which nodes have been explored
-    # explored = np.zeros(nNodes, dtype=np.bool)
-    #
-    # for iNode in range(nNodes):
-    #     if not explored[iNode]:
-    #         # call DFS, but with the rows (ie, direction) of the edge list reversed
-    #         DFS_fwd(G[:, ::-1], iNode, t, f, explored)
+    # initialize array that will hold the leaders of all nodes
+    leaders = np.zeros(nNodes, dtype=np.int32)
+
+    # Initialize an array to keep track of which nodes have been explored
+    explored = np.zeros(nNodes, dtype=np.bool)
+
+    for iNode in range(nNodes):
+        if not explored[iNode]:
+            # call DFS
+            DFS_fwd(G, iNode, leaders, explored)
 
     return leaders
+
+
+def DFS_fwd(G, s, leaders, explored):
+
+    stacksize = 1000
+    stack = np.zeros(stacksize, dtype=np.int32)
+
+    stack[0] = s
+    iStack = 0
+
+    while iStack >= 0:
+
+        iNode = stack[iStack]
+        explored[iNode] = True
+        leaders[iNode] = s
+
+        iStack -= 1
+
+        for targetNode in G[G[:,0]==iNode][:,1]:
+            if not explored[targetNode]:
+                iStack += 1
+
+                if iStack >=stacksize:
+                    raise NameError("Ran out of stack space")
+
+                stack[iStack] = targetNode
+
 
--- main.py
+++ main.py
@@ -5,11 +5,20 @@ from KosarajuLib import kosaraju
 
 #  Get the graph from file
 # SCC_filename = "SCCsmall"
-SCC_filename = "SCCsmall"
+SCC_filename = "SCC"
 G = get_graph(SCC_filename)
 
 # Perform the Kosaraju algorithm to get the leaders for each node.
 # (the nodes reporting a shared leader will belong to the same SCC).
 leaders = kosaraju(G)
+nLeaders = leaders.max()+1
+SCC_sizes = np.zeros(nLeaders,dtype=np.int32)
+
+for ldr in range(nLeaders):
+    SCC_sizes[ldr] = sum(leaders==ldr)
+
+SCC_sizes[::-1].sort()
+print(SCC_sizes[:5])
+
 
 print('all done.')
